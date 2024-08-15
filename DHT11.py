71f4cb410551332594e2e68910214cdd6d53350a
pml0607 <phammanhlinh680@gmail.com>
2024-08-16 04:34:25 +0700

    a

--- DHT11.py
+++ DHT11.py
@@ -3,7 +3,7 @@ import board
 import adafruit_dht
 
 # Sensor data pin is connected to GPIO 4
-sensor = adafruit_dht.DHT22(board.D4)
+sensor = adafruit_dht.DHT11(board.D4)
 # Uncomment for DHT11
 #sensor = adafruit_dht.DHT11(board.D4)
 
